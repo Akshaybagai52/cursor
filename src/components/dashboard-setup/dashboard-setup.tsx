db87254d60d9cc2074703a095915fe9bbd15139b
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2024-02-01 23:02:30 +0530

    feat: setup dashboard page

---
+++ src/components/dashboard-setup/dashboard-setup.tsx
@@ -0,0 +1,27 @@
+import { AuthUser } from '@supabase/supabase-js';
+import React from 'react'
+import { Card, CardDescription, CardHeader, CardTitle } from '../ui/card';
+
+interface DashboardSetupProps {
+    user: AuthUser;
+    subscription: {} | null;
+}
+
+const DashboardSetup: React.FC<DashboardSetupProps> = ({
+    subscription,
+    user,
+}) => {
+  return (
+    <Card className='w-[800px] h-screen sm:h-auto'>
+        <CardHeader>
+            <CardTitle>Create A Workspace</CardTitle>
+            <CardDescription>
+                Lets create a private workspace to get you started, You can add 
+                collaborators later from the workspace settings tab.
+            </CardDescription>
+        </CardHeader>
+    </Card>
+  )
+}
+
+export default DashboardSetup
